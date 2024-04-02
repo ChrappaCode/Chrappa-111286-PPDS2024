e1da6abefd9e05c0d4361d81f7161fbca1b9aa1c
chrappa <kubino321@gmail.com>
2024-04-02 18:00:04 +0200

    ADDED: analýzy

---
+++ Kolektivna_analyza.py
@@ -0,0 +1,69 @@
+import numpy as np
+from mpi4py import MPI
+import matplotlib.pyplot as plt
+
+NRA = 10  # number of rows in matrix A
+NCA = 3   # number of columns in matrix A
+NCB = 7   # number of columns in matrix B
+
+MASTER = 0
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nproc = comm.Get_size()
+
+times = []
+
+def custom_matrix_multiply(A, B):
+    # Custom matrix multiplication logic
+    nrows, ncols = A.shape[0], B.shape[1]
+    C = np.zeros((nrows, ncols), dtype=int)
+    for i in range(nrows):
+        for j in range(ncols):
+            for k in range(NCA):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
+
+for _ in range(50):  # Run the code 50 times
+    if rank == MASTER:
+        start_time = MPI.Wtime()
+
+    # Initialization
+    rows_per_proc = NRA // nproc
+    extra_rows = NRA % nproc
+
+    if rank == MASTER:
+        A = np.array([i + j for j in range(NRA) for i in range(NCA)]).reshape(NRA, NCA)
+        B = np.array([i + j for j in range(NCA) for i in range(NCB)]).reshape(NCA, NCB)
+        C = np.zeros((NRA, NCB), dtype=int)
+    else:
+        A = None
+        B = None
+        C = None
+
+    A_loc = np.zeros((rows_per_proc + (1 if rank < extra_rows else 0), NCA), dtype=int)
+
+    # Scatter matrix A
+    comm.Scatter([A, MPI.INT], [A_loc, MPI.INT])
+
+    # Broadcast matrix B
+    B = comm.bcast(B, root=MASTER)
+
+    C_loc = custom_matrix_multiply(A_loc, B)
+
+    # Gather the results
+    C = None
+    if rank == MASTER:
+        C = np.zeros((NRA, NCB), dtype=int)
+    comm.Gather([C_loc, MPI.INT], [C, MPI.INT], root=MASTER)
+
+    # Print the result at MASTER
+    if rank == MASTER:
+        end_time = MPI.Wtime()
+        times.append(end_time - start_time)
+
+if rank == MASTER:
+    plt.hist(times, bins=10)
+    plt.xlabel('Execution Time (seconds)')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Execution Times')
+    plt.show()
---
+++ P2P_analyza.py
@@ -0,0 +1,75 @@
+import numpy as np
+from mpi4py import MPI
+import matplotlib.pyplot as plt
+
+NRA = 10  # number of rows in matrix A
+NCA = 3   # number of columns in matrix A
+NCB = 7   # number of columns in matrix B
+
+MASTER = 0
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nproc = comm.Get_size()
+
+times = []
+
+for _ in range(50):  # Run the code 50 times
+    if rank == MASTER:
+        start_time = MPI.Wtime()
+
+    # Initialization
+    rows_per_proc = NRA // nproc
+    extra_rows = NRA % nproc
+
+    if rank != MASTER:
+        A_loc = None
+        B = None
+    else:
+        A = np.array([i + j for j in range(NRA) for i in range(NCA)]).reshape(NRA, NCA)
+        B = np.array([i + j for j in range(NCA) for i in range(NCB)]).reshape(NCA, NCB)
+        C = np.zeros((NRA, NCB), dtype=int)
+
+        ind = 0
+        for proc in range(nproc):
+            if proc == MASTER:
+                A_loc = A[ind:ind+rows_per_proc + (1 if proc < extra_rows else 0)]
+                ind += rows_per_proc + (1 if proc < extra_rows else 0)
+                continue
+            comm.send(A[ind:ind+rows_per_proc + (1 if proc < extra_rows else 0)], dest=proc)
+            ind += rows_per_proc + (1 if proc < extra_rows else 0)
+
+    B = comm.bcast(B, root=MASTER)
+
+    C_loc = np.zeros((rows_per_proc + (1 if rank < extra_rows else 0), NCB), dtype=int)
+
+    if rank != MASTER:
+        C_loc = np.dot(comm.recv(), B)
+    else:
+        for i in range(len(C_loc)):
+            for j in range(NCB):
+                for k in range(NCA):
+                    C_loc[i][j] += A_loc[i][k] * B[k][j]
+
+    if rank != MASTER:
+        comm.send(C_loc, dest=MASTER)
+    else:
+        ind = 0
+        for proc in range(nproc):
+            if proc == MASTER:
+                C[ind:ind+rows_per_proc + (1 if proc < extra_rows else 0)] = C_loc
+                ind += rows_per_proc + (1 if proc < extra_rows else 0)
+                continue
+            C_loc = comm.recv(source=proc)
+            C[ind:ind + len(C_loc)] = C_loc
+            ind += len(C_loc)
+
+        end_time = MPI.Wtime()
+        times.append(end_time - start_time)
+
+if rank == MASTER:
+    print(times)
+    plt.hist(times, bins=10)
+    plt.xlabel('Execution Time (seconds)')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of Execution Times')
+    plt.show()
