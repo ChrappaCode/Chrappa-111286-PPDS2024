0066fd5e1039a6df9e395e858c6765676f5e3ba5
chrappa <kubino321@gmail.com>
2024-04-05 08:49:41 +0200

    FIX: MASTER

--- Kolektivna_kom.py
+++ Kolektivna_kom.py
@@ -5,7 +5,6 @@ NRA = 10  # number of rows in matrix A
 NCA = 3   # number of columns in matrix A
 NCB = 7   # number of columns in matrix B
 
-MASTER = 0
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 nproc = comm.Get_size()
@@ -20,8 +19,7 @@ def custom_matrix_multiply(A, B):
                 C[i][j] += A[i][k] * B[k][j]
     return C
 
-if rank == MASTER:
-    start_time = MPI.Wtime()
+start_time = MPI.Wtime()
 
 print("Paralelne násobenie matíc..")
 print(f"Matrix sizes A[{NRA}][{NCA}], B[{NCA}][{NCB}], C[{NRA}][{NCB}]")
@@ -32,35 +30,23 @@ print("Inicializácia..")
 rows_per_proc = NRA // nproc
 extra_rows = NRA % nproc
 
-if rank == MASTER:
-    A = np.array([i + j for j in range(NRA) for i in range(NCA)]).reshape(NRA, NCA)
-    B = np.array([i + j for j in range(NCA) for i in range(NCB)]).reshape(NCA, NCB)
-    C = np.zeros((NRA, NCB), dtype=int)
-else:
-    A = None
-    B = None
-    C = None
-
 A_loc = np.zeros((rows_per_proc + (1 if rank < extra_rows else 0), NCA), dtype=int)
 
 # Scatter matrix A
-comm.Scatter([A, MPI.INT], [A_loc, MPI.INT])
+comm.Scatter([None if rank != 0 else np.array([i + j for j in range(NRA) for i in range(NCA)]).reshape(NRA, NCA), MPI.INT], [A_loc, MPI.INT])
 
-B = comm.bcast(B, root=MASTER)
+B = comm.bcast(None if rank != 0 else np.array([i + j for j in range(NCA) for i in range(NCB)]).reshape(NCA, NCB), root=0)
 
 print("Nasobenie matíc..")
 C_loc = custom_matrix_multiply(A_loc, B)
 
 # Gather the results
-C = None
-if rank == MASTER:
-    C = np.zeros((NRA, NCB), dtype=int)
-comm.Gather([C_loc, MPI.INT], [C, MPI.INT], root=MASTER)
+C = comm.gather(C_loc, root=0)
 
 # Print the result at MASTER
-if rank == MASTER:
+if rank == 0:
     print("Vysledok nasobenia:")
-    print(C)
+    print(np.concatenate(C))
     end_time = MPI.Wtime()
     print("Čas:", end_time - start_time, "sekúnd")
 
